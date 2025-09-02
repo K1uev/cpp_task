28c4343c6e250d987567417d5828ae693ad518c7
K1uev <109747646+K1uev@users.noreply.github.com>
2025-09-02 08:48:05 +0300

    Исправленные файлы
    Внес правки в файлы в соответствии с замечаниями: добавлены тесты и main был разбит на небольшие функции

--- Task1Option1/copyfile.cpp
+++ Task1Option1/copyfile.cpp
@@ -1,33 +1,81 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unistd.h>
+
+bool checkCorrectUsage(int argc, char* argv[]) {
+	if (argc != 3) {
+		std::cerr << "Правильное использование: " << argv[0] << " <input file name> <output file name>" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool checkFileAccess(const std::string& filename) {
+	if (access(filename.c_str(), F_OK) != 0) {
+		std::cerr << "Файл '" << filename << "' не существует" << std::endl;
+		return false;
+	}
+
+	if (access(filename.c_str(), R_OK) != 0) {
+		std::cerr << "Нет прав на чтнение файла '" << filename << "'" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool checkOpenedInputFile(std::ifstream& inFile, const std::string& inputFilename) {
+	if (!checkFileAccess(inputFilename)) {
+		return false;
+	}
+
+	inFile.open(inputFilename);
+	if (!inFile.is_open()) {
+		std::cerr << "Ошибка открытия файла: " << inputFilename << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool checkOpenedOutputFile(std::ofstream& outFile, const std::string& outputFilename) {
+	outFile.open(outputFilename);
+	if (!outFile.is_open()) {
+		std::cerr << "Ошибка создания файла: " << outputFilename << std::endl;
+		return false;
+	}
+	return true;
+}
+
+void copyFile(std::ifstream& inFile, std::ofstream& outFile) {
+	std::string line;
+	while (std::getline(inFile, line)) {
+		outFile << line << std::endl;
+	}
+}
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        std::cerr << "Правильное использование: copyfile.exe <input file name> <output file name>" << std::endl;
-        return 1;
-    }
+	if (!checkCorrectUsage(argc, argv)) {
+		return 1;
+	}
 
-    const std::string inputFilename(argv[1]);
-    const std::string outputFilename(argv[2]);
+	const std::string inputFilename(argv[1]);
+	const std::string outputFilename(argv[2]);
 
-    std::ifstream inFile(inputFilename);
-    if (!inFile.is_open()) {
-        std::cerr << "Ошибка открытия файла: " << inputFilename << std::endl;
-        return 1;
-    }
+	std::ifstream inFile;
+	if (!checkOpenedInputFile(inFile, inputFilename)) {
+		return 1;
+	}
 
-    std::ofstream outFile(outputFilename);
-    if (!outFile.is_open()) {
-        std::cerr << "Ошибка создания файла: " << outputFilename << std::endl;
-        return 1;
-    }
+	std::ofstream outFile;
+	if (!checkOpenedOutputFile(outFile, outputFilename)) {
+		return 1;
+	}
 
-    std::string line;
-    while (std::getline(inFile, line)) {
-        outFile << line << std::endl;
-    }
+	copyFile(inFile, outFile);
 
-    std::cout << "Копирование '" << inputFilename << "' в '" << outputFilename << "' прошло успешно" << std::endl;
-    return 0;
+	inFile.close();
+	outFile.close();
+
+	std::cout << "Копирование '" << inputFilename << "' в '" << outputFilename << "' прошло успешно" << std::endl;
+	return 0;
 }
--- Task1Option1/test.sh
+++ Task1Option1/test.sh
@@ -3,21 +3,50 @@
 clang++ -o copyfile.exe copyfile.cpp
 
 echo "Тест 1: Проверка нормальной работы"
+touch test-output.txt
 ./copyfile.exe test-input.txt test-output.txt
 diff test-input.txt test-output.txt && echo "Тест пройден!"
 rm test-output.txt
 echo
 
-echo "Тест 2: Тестирование отсутствующего входного файла..."
+echo "Тест 2: Копирование пустого файла"
+touch test-output.txt
+./copyfile.exe empty-file.txt test-output.txt
+if [[ -s test-output.txt ]]; then
+    echo "Тест провален!"
+else
+    diff empty-file.txt test-output.txt && echo "Тест пройден!"
+fi
+rm test-output.txt
+echo
+
+echo "Тест 3: Тестирование файла без прав доступа"
+touch test-output.txt
+echo "Unavailable value" > unavailable-file.txt
+chmod 000 unavailable-file.txt
+./copyfile.exe unavailable-file.txt test-output.txt
+if [[ "$?" -ne 0 ]]; then
+    echo "Тест пройден!"
+else
+    echo "Тест провален!"
+fi
+chmod 644 unavailable-file.txt
+rm unavailable-file.txt
+rm test-output.txt
+echo
+
+echo "Тест 4: Тестирование отсутствующего входного файла"
+touch test-output.txt
 ./copyfile.exe nofile.txt test-output.txt
 if [[ "$?" -ne 0 ]]; then
     echo "Тест пройден!"
 else
     echo "Тест провален!"
 fi
+rm test-output.txt
 echo
 
-echo "Тест 3: Тестирование неправильного использования..."
+echo "Тест 5: Тестирование неправильного использования"
 ./copyfile.exe
 if [[ "$?" -ne 0 ]]; then
     echo "Тест пройден!"
