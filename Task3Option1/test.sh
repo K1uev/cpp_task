09b1169e47d2c5b165d09eac3118168b8834cea2
K1uev <109747646+K1uev@users.noreply.github.com>
2025-09-05 21:20:16 +0300

    Внесены правки

--- Task3Option1/test.sh
+++ Task3Option1/test.sh
@@ -2,20 +2,49 @@
 
 clang++ -o wordCount.exe wordCount.cpp
 
-echo "Тест 1: 'яблоко яблоко груша'"
-echo -e "яблоко яблоко груша" | ./wordCount.exe
+checkResults() {
+	expectedValue="$1"
+	result="$2"
+
+	if [ "$(echo "$expectedValue" | xargs)" = "$(echo "$result" | xargs)" ]; then
+		echo "Тест пройден"
+	else
+		echo "Ошибка"
+		echo "Ожидаемое значение:"
+		echo "$expectedValue"
+		echo "Полученное значение:"
+		echo "$result"
+	fi
+}
+
+echo "Тест 1: Передача строки 'яблоко яблоко груша'"
+expectedValue="груша->1
+яблоко->2"
+result=$(echo -e "яблоко яблоко груша" | ./wordCount.exe)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 2: 'груша дыня Яблоко яблоко груша'"
-echo -e "груша дыня Яблоко яблоко груша" | ./wordCount.exe
+echo "Тест 2: Передача строки 'груша дыня Яблоко яблоко груша'"
+expectedValue="Яблоко->1
+груша->2
+дыня->1
+яблоко->1"
+result=$(echo -e "груша дыня Яблоко яблоко груша" | ./wordCount.exe)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 3: '3 1 2 3 2 3'"
-echo -e "3 1 2 3 2 3" | ./wordCount.exe
+echo "Тест 3: Передача строки '3 1 2 3 2 3'"
+expectedValue="1->1
+2->2
+3->3"
+result=$(echo -e "3 1 2 3 2 3" | ./wordCount.exe)
+checkResults "$expectedValue" "$result"
 echo
 
 echo "Тест 4: пустая строка"
-echo -e "" | ./wordCount.exe
+expectedValue=""
+result=$(echo -e "" | ./wordCount.exe)
+checkResults "$expectedValue" "$result"
 echo
 
 rm wordCount.exe
--- Task3Option1/wordCount.cpp
+++ Task3Option1/wordCount.cpp
@@ -8,19 +8,19 @@ int main()
 	std::string line;
 	std::map<std::string, int> countWordsMap;
 
-	while(std::getline(std::cin, line))
+	while (std::getline(std::cin, line))
 	{
 		std::istringstream inputStr(line);
 		std::string word;
 
-		while(inputStr >> word)
+		while (inputStr >> word)
 		{
 			countWordsMap[word]++;
 		}
 	}
 
 	auto mapElemCount = countWordsMap.begin();
-	while(mapElemCount != countWordsMap.end())
+	while (mapElemCount != countWordsMap.end())
 	{
 		std::cout << mapElemCount->first << "->" << mapElemCount->second << std::endl;
 		++mapElemCount;
