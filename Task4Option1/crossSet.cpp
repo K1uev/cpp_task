ce1a6c2aa0331a90985ee0c050332e5019afefab
K1uev <109747646+K1uev@users.noreply.github.com>
2025-09-05 21:20:42 +0300

    Внесены правки

--- Task4Option1/crossSet.cpp
+++ Task4Option1/crossSet.cpp
@@ -5,13 +5,17 @@
 #include <sstream>
 #include <iterator>
 
-bool isValidInput(const std::string& input) {
-	if (input.empty()) {
+bool isValidInput(const std::string& input)
+{
+	if (input.empty())
+    {
 	return false;
 	}
 	
-	for (char inputChar : input) {
-		if (!std::isdigit(inputChar)) {
+	for (char inputChar : input)
+    {
+		if (!std::isdigit(inputChar))
+        {
 		return false;
 		}
 	}
@@ -19,10 +23,12 @@ bool isValidInput(const std::string& input) {
 	return true;
 }
 
-int sumEachNum(int num) {
+int sumEachNum(int num)
+{
 	int sum = 0;
 	
-	while (num > 0) {
+	while (num > 0)
+    {
 		sum += num % 10;
 		num /= 10;
 	}
@@ -30,11 +36,14 @@ int sumEachNum(int num) {
 	return sum;
 }
 
-std::set<int> createSetEachNumDevideSumNum(int lastNum) {
+std::set<int> createSetEachNumDevideSumNum(int lastNum)
+{
 	std::set<int> setDevideSumNum;
-	for (int i = 1; i <= lastNum; ++i) {
+	for (int i = 1; i <= lastNum; ++i)
+    {
 		int sum = sumEachNum(i);
-		if (sum != 0 && i % sum == 0) {
+		if (sum != 0 && i % sum == 0)
+        {
 			setDevideSumNum.insert(i);
 		}
 	}
@@ -42,10 +51,13 @@ std::set<int> createSetEachNumDevideSumNum(int lastNum) {
 	return setDevideSumNum;
 }
 
-std::set<int> createSetSumEven(int lastNum) {
+std::set<int> createSetSumEven(int lastNum)
+{
 	std::set<int> setSumEven;
-	for (int i = 1; i <= lastNum; ++i) {
-		if (sumEachNum(i) % 2 == 0) {
+	for (int i = 1; i <= lastNum; ++i)
+    {
+		if (sumEachNum(i) % 2 == 0)
+        {
 			setSumEven.insert(i);
 		}
 	}
@@ -53,33 +65,46 @@ std::set<int> createSetSumEven(int lastNum) {
 	return setSumEven;
 }
 
-bool checkEmptySet(const std::set<int>& printedSet) {
-	if(printedSet.empty()) {
+bool checkEmptySet(const std::set<int>& printedSet)
+{
+	if(printedSet.empty())
+    {
 		return false;
 	}
 
 	return true;
 }
 
-void printSet(const std::set<int>& printedSet) {
-	if(checkEmptySet(printedSet)) {
-		for (auto i = printedSet.begin(); i != printedSet.end(); ++i) {
+void printSet(const std::set<int>& printedSet)
+{
+	if (checkEmptySet(printedSet)) {
+		for (auto i = printedSet.begin(); i != printedSet.end(); ++i)
+        {
 			std::cout << *i << " ";
 		}
 		std::cout << std::endl;
 	}
 }
 
-std::set<int> findSetCross(std::set<int> const& set1, std::set<int> const& set2) {
+std::set<int> findSetCross(std::set<int> const& set1, std::set<int> const& set2)
+{
 	std::set<int> setCross;
-	std::set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(), std::inserter(setCross, setCross.begin()));
+	std::set_intersection(set1.begin(), set1.end(), 
+                            set2.begin(), set2.end(), 
+                            std::inserter(setCross, setCross.begin()));
 
 	return setCross;
 }
 
-int main(int argc, char* argv[]) {
-	if (argc == 2 && std::string(argv[1]) == "-h") {
-		std::cout << "Программа возвращает два множества:\nПервое множество – множество чисел от 1 до N, делящихся без остатка на сумму своих цифр;\nВторое множество – множество целых чисел то 1 до N, сумма цифр которых является четной.\nНеобходимо передать положительное число или \"-h\" для получения справки" << std::endl;
+int main(int argc, char* argv[]) 
+{
+	if (argc == 2 && std::string(argv[1]) == "-h") 
+    {
+		std::cout << "Программа возвращает два множества:\n" 
+                << "Первое множество – множество чисел от 1 до N, делящихся без остатка на сумму своих цифр;\n"
+                << "Второе множество – множество целых чисел то 1 до N, сумма цифр которых является четной.\n"
+                << "Необходимо передать положительное число или \"-h\" для получения справки"
+                << std::endl;
 
 		return 0;
 	}
@@ -87,24 +112,32 @@ int main(int argc, char* argv[]) {
 	std::string input;
 	int lastNum;
 	
-	if (argc == 2) {
+	if (argc == 2) 
+    {
 		input = argv[1];
-		if (!isValidInput(input)) {
-			std::cerr << "ERROR" << std::endl;
+		if (!isValidInput(input)) 
+        {
+			std::cout << "ERROR" << std::endl;
+
 			return 1;
 		}
 		lastNum = std::stoi(input);
 	}
-	else if (argc == 1) {
+	else if (argc == 1)
+    {
 		std::getline(std::cin, input);
-		if (!isValidInput(input)) {
-			std::cerr << "ERROR" << std::endl;
+		if (!isValidInput(input))
+        {
+			std::cout << "ERROR" << std::endl;
+
 			return 1;
 		}
 		lastNum = std::stoi(input);
 	}
-	else {
-		std::cerr << "ERROR" << std::endl;
+	else
+    {
+		std::cout << "ERROR" << std::endl;
+        
 		return 1;
 	}
 	
--- Task4Option1/test.sh
+++ Task4Option1/test.sh
@@ -2,109 +2,110 @@
 
 clang++ -o crossSet.exe crossSet.cpp
 
-echo "Тест 1:
-Переданное значение (режим stdin): 10; 
-Ожидаемый результат: 
-1 2 3 4 5 6 7 8 9 10 
+checkResults() {
+	expectedValue="$1"
+	result="$2"
+
+	if [ "$(echo "$expectedValue" | xargs)" = "$(echo "$result" | xargs)" ]; then
+		echo "Тест пройден"
+	else
+		echo "Ошибка"
+		echo "Ожидаемое значение:"
+		echo "$expectedValue"
+		echo "Полученное значение:"
+		echo "$result"
+	fi
+}
+
+echo "Тест 1: Переданное значение (режим stdin): 10"
+expectedValue="1 2 3 4 5 6 7 8 9 10 
 2 4 6 8 
-2 4 6 8
-"
-echo -e "10" | ./crossSet.exe
+2 4 6 8"
+result=$(echo -e "10" | ./crossSet.exe)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 2:
-Переданное значение (режим stdin): 5; 
-Ожидаемый результат: 
-1 2 3 4 5 
+echo "Тест 2: Переданное значение (режим stdin): 5"
+expectedValue="1 2 3 4 5 
 2 4 
-2 4
-"
-echo -e "5" | ./crossSet.exe
+2 4"
+result=$(echo -e "5" | ./crossSet.exe)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 3: Переданное значение (режим stdin): 20; 
-Ожидаемый результат: 
-1 2 3 4 5 6 7 8 9 10 12 18 20 
+echo "Тест 3: Переданное значение (режим stdin): 20"
+expectedValue="1 2 3 4 5 6 7 8 9 10 12 18 20 
 2 4 6 8 11 13 15 17 19 20 
-2 4 6 8 20 
-"
-echo -e "20" | ./crossSet.exe
+2 4 6 8 20"
+result=$(echo -e "20" | ./crossSet.exe)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 4: Переданное значение (режим stdin): 1; 
-Ожидаемый результат: 
-1 
-"
-echo -e "1" | ./crossSet.exe
+echo "Тест 4: Переданное значение (режим stdin): 1"
+expectedValue="1"
+result=$(echo -e "1" | ./crossSet.exe)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 5: Переданное значение (режим stdin): 1; 
-Ожидаемый результат: 
-1 2 3 4 5 6 7 8 9 10 12 18 20 21 24 27 30 36 40 42 45 48 50 
+echo "Тест 5: Переданное значение (режим stdin): 50"
+expectedValue="1 2 3 4 5 6 7 8 9 10 12 18 20 21 24 27 30 36 40 42 45 48 50 
 2 4 6 8 11 13 15 17 19 20 22 24 26 28 31 33 35 37 39 40 42 44 46 48 
-2 4 6 8 20 24 40 42 48 
-"
-echo -e "50" | ./crossSet.exe
+2 4 6 8 20 24 40 42 48"
+result=$(echo -e "50" | ./crossSet.exe)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 6: Переданное значение (режим командной строки): 15; 
-Ожидаемый результат: 
-1 2 3 4 5 6 7 8 9 10 12 
+echo "Тест 6: Переданное значение (режим командной строки): 15"
+expectedValue="1 2 3 4 5 6 7 8 9 10 12 
 2 4 6 8 11 13 15 
-2 4 6 8
-"
-./crossSet.exe 15
+2 4 6 8"
+result=$(./crossSet.exe 15)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 7: Переданное значение: -h
-Ожидаемый результат: 
-Программа возвращает два множества:
+echo "Тест 7: Переданное значение: -h"
+expectedValue="Программа возвращает два множества:
 Первое множество – множество чисел от 1 до N, делящихся без остатка на сумму своих цифр;
 Второе множество – множество целых чисел то 1 до N, сумма цифр которых является четной.
-Необходимо передать положительное число или \"-h\" для получения справки
-"
-./crossSet.exe -h
+Необходимо передать положительное число или \"-h\" для получения справки"
+result=$(./crossSet.exe -h)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 8: Переданное значение (режим stdin): \"\"; 
-Ожидаемый результат:
-ERROR
-"
-echo -e "" | ./crossSet.exe
+echo "Тест 8: Переданное значение (режим stdin): пустая строка"
+expectedValue="ERROR"
+result=$(echo -e "" | ./crossSet.exe)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 9: Переданное значение (режим stdin): test; 
-Ожидаемый результат:
-ERROR"
-echo -e "test" | ./crossSet.exe
+echo "Тест 9: Переданное значение (режим stdin): test"
+expectedValue="ERROR"
+result=$(echo -e "test" | ./crossSet.exe)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 10: Переданное значение (режим командной строки): test; 
-Ожидаемый результат:
-ERROR
-"
-./crossSet.exe test
+echo "Тест 10: Переданное значение (режим командной строки): test"
+expectedValue="ERROR"
+result=$(./crossSet.exe test)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 11: Переданное значение (режим командной строки): -1; 
-Ожидаемый результат:
-ERROR
-"
-./crossSet.exe -1
+echo "Тест 11: Переданное значение (режим командной строки): -1"
+expectedValue="ERROR"
+result=$(./crossSet.exe -1)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 12: Переданное значение (режим командной строки): 0; 
-Ожидаемый результат:
-*пустота*
-"
-./crossSet.exe 0
+echo "Тест 12: Переданное значение (режим командной строки): 0"
+expectedValue=""
+result=$(./crossSet.exe 0)
+checkResults "$expectedValue" "$result"
 echo
 
-echo "Тест 13: Переданное значение (режим командной строки): 1 2; 
-Ожидаемый результат:
-ERROR
-"
-./crossSet.exe 1 2
+echo "Тест 13: Переданное значение (режим командной строки): 1 2"
+expectedValue="ERROR"
+result=$(./crossSet.exe 1 2)
+checkResults "$expectedValue" "$result"
 echo
 
 rm crossSet.exe
