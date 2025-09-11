a5173c172cb112755bcf4b3c5a0736b36495db1a
K1uev <109747646+K1uev@users.noreply.github.com>
2025-09-11 08:09:36 +0300

    Внесены правки после замечаний по 1 лабе

--- Task1Option2/multiply_each_element.cpp
+++ Task1Option2/multiply_each_element.cpp
@@ -1,13 +1,12 @@
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
 #include <vector>
-#include <iomanip>
-#include <algorithm>
-
 
 std::vector<double> ReadNumbers(int argc, char* argv[])
 {
-	std::vector<double> newVector;
-	
+	std::vector<double> newVector{};
+
 	if (argc <= 1)
 	{
 		return newVector;
@@ -23,25 +22,25 @@ std::vector<double> ReadNumbers(int argc, char* argv[])
 		catch (const std::exception& e)
 		{
 			std::cout << "ERROR" << std::endl;
-			return std::vector<double> {};
+			return std::vector<double>{};
 		}
 	}
 
 	return newVector;
-} 
+}
 
-std::vector<double> ProcessNumbers(std::vector<double> dValue)
+std::vector<double> ProcessNumbers(const std::vector<double>& dValue)
 {
-	std::vector<double> newVectorUpdated;
+	std::vector<double> newVectorUpdated{};
 
-	if (dValue.size() == 0)
+	if (dValue.empty())
 	{
 		return newVectorUpdated;
 	}
 
-	auto minIt = std::min_element(dValue.begin(), dValue.end());
-	double minValue = *minIt;
-	
+	auto minEl = std::min_element(dValue.begin(), dValue.end());
+	double minValue = *minEl;
+
 	for (size_t i = 0; i < dValue.size(); i++)
 	{
 		newVectorUpdated.push_back(dValue[i] * minValue);
@@ -50,19 +49,19 @@ std::vector<double> ProcessNumbers(std::vector<double> dValue)
 	return newVectorUpdated;
 }
 
-void PrintSortedNumbers(std::vector<double> dValue)
+void PrintSortedNumbers(const std::vector<double>& dValue)
 {
-	if (dValue.size() == 0)
+	if (dValue.empty())
 	{
 		std::cout << std::endl;
 		return;
 	}
+	std::vector<double> sortedValue = dValue;
+	std::sort(sortedValue.begin(), sortedValue.end());
 
-	std::sort(dValue.begin(), dValue.end());
-
-	for (size_t i = 0; i < dValue.size(); i++)
+	for (size_t i = 0; i < sortedValue.size(); i++)
 	{
-		std::cout << std::fixed << std::setprecision(3) << dValue[i] << " ";
+		std::cout << std::fixed << std::setprecision(3) << sortedValue[i] << " ";
 	}
 	std::cout << std::endl;
 }
@@ -72,21 +71,6 @@ int main(int argc, char* argv[])
 	std::vector<double> readNumbers = ReadNumbers(argc, argv);
 	std::vector<double> processNumbers = ProcessNumbers(readNumbers);
 	PrintSortedNumbers(processNumbers);
-    
+
 	return 0;
 }
-
-
-
-/*
-Выводится результат в порядке убывания, но по заданию должен выводиться в порядке неубывания
-
-Пример 2
-Ввод:
-
-10 7 4
-Вывод:
-
-40.000 28.000 16.000
-Объяснение: минимальный элемент - 4, умножаем каждый элемент массива на 4.
-*/
